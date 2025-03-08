2cf6c8feda2e0708da2ce5160106e48a965aadd3
suef606 <sufe3005@gmail.com>
2025-03-09 05:13:22 +0900

    section3-3_UI구축

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import UI from "./ui";
 
 export default function Home() {
   return (
     <main>
-      TODO LIST <i className="fas fa-check"></i>
+      <UI />
     </main>
   );
 }
